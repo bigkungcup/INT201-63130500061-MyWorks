ccb24c469f4bd779e933bcb1d82c9e8b55d34083
BlackCloudy <68818040+bigkungcup@users.noreply.github.com>
2021-09-17 21:00:37 +0700

    Add files via upload

---
+++ Week3/Lab03.js
@@ -0,0 +1,149 @@
+function zodiacYear(years){
+    let zodiac = years%12
+
+    switch(zodiac){
+
+        case 0:console.log("Monkey")
+        break;
+        
+        case 1:console.log("Rooster")
+        break;
+        
+        case 2:console.log("Dog")
+        break;
+        
+        case 3:console.log("Pig")
+        break;
+        
+        case 4:console.log("Rat")
+        break;
+        
+        case 5:console.log("Ox")
+        break;
+        
+        case 6:console.log("Tiger")
+        break;
+        
+        case 7:console.log("Rabbit")
+        break;
+        
+        case 8:console.log("Dragon")
+        break;
+        
+        case 9:console.log("Snake")
+        break;
+        
+        case 10:console.log("Horse")
+        break;
+        
+        case 11:console.log("Sheep")
+        break;
+
+    }return zodiac;
+}
+    zodiacYear(2001);
+
+
+
+let years = 2001;
+
+    switch(years%12){
+
+        case 0:console.log("Monkey")
+        break;
+
+        case 1:console.log("Rooster")
+        break;
+
+        case 2:console.log("Dog")
+        break;
+
+        case 3:console.log("Pig")
+        break;
+
+        case 4:console.log("Rat")
+        break;
+
+        case 5:console.log("Ox")
+        break;
+
+        case 6:console.log("Tiger")
+        break;
+
+        case 7:console.log("Rabbit")
+        break;
+
+        case 8:console.log("Dragon")
+        break;
+
+        case 9:console.log("Snake")
+        break;
+
+        case 10:console.log("Horse")
+        break;
+
+        case 11:console.log("Sheep")
+        break;
+    }return 
+
+// console.log(years);
+
+
+
+const Zodiac = {0:"Monkey",1:"Rooster",2:"Dog",3:"Pig",4:"Rat",5:"Ox",
+                6:"Tiger",7:"Rabbit",8:"Dragon",9:"Snake",10:"Horse",
+                11:"Sheep"}
+
+let years = 2001;
+
+        switch(years%12){
+            case 0:
+                console.log(Zodiac[0])
+                break;
+
+            case 1:
+                console.log(Zodiac[1])
+                break;
+
+            case 2:
+                console.log(Zodiac[2])
+                break;
+            
+            case 3:
+                console.log(Zodiac[3])
+                break;
+            
+            case 4:
+                console.log(Zodiac[4])
+                break;
+
+            case 5:
+                console.log(Zodiac[5])
+                break;
+
+            case 6:
+                console.log(Zodiac[6])
+                break;
+
+            case 7:
+                console.log(Zodiac[7])
+                break;
+            
+            case 8:
+                console.log(Zodiac[8])
+                break;
+
+            case 9:
+                console.log(Zodiac[9])
+                break;
+            
+            case 10:
+                console.log(Zodiac[10])
+                break;
+            
+            case 11:
+                console.log(Zodiac[11])
+                break;
+        }
+
+// console.log(years);
